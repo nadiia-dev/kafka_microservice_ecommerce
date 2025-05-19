9557f0ef54744dc09de4fdb4a673be0f3ae85968
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-05-19 15:08:25 +0300

    connected db to order service via drizzle

---
+++ order_service/drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+import { DB_URL } from "./src/config";
+
+export default defineConfig({
+  out: "./src/db/migrations",
+  schema: "./src/db/schema/*",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: DB_URL as string,
+  },
+});
--- order_service/src/repository/cart.repository.ts
+++ order_service/src/repository/cart.repository.ts
@@ -1,13 +1,22 @@
+import { DB } from "../db/db.connections";
+import { carts } from "../db/schema";
+
 export type CartRepositoryType = {
-  createCart: (customerId: number, lineItem: any) => Promise<number>;
-  findCart: (id: number) => Promise<any>;
+  createCart: (customerId: number, lineItem: any) => Promise<{}>;
+  getCart: (id: number) => Promise<any>;
   updateCart: (id: number, qty: number) => Promise<any>;
   deleteCart: (id: number) => Promise<Boolean>;
   clearCartData: (id: number) => Promise<Boolean>;
   findCartByProductId: (customerId: number, productId: number) => Promise<any>;
 };
 
-const createCart = (customerId: number, lineItem: any) => {};
+const createCart = async (customerId: number, lineItem: any): Promise<{}> => {
+  const res = await DB.insert(carts)
+    .values({ customerId })
+    .returning({ cartId: carts.id });
+
+  return Promise.resolve({ message: "Created" });
+};
 
 const getCart = (id: number) => {};
 
