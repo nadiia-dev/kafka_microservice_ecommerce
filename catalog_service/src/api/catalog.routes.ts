713f9b78c6eb7eef88637b9e7fd1445adcd39b8e
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-05-19 12:16:10 +0300

    implemented catalog routes

--- catalog_service/src/api/catalog.routes.ts
+++ catalog_service/src/api/catalog.routes.ts
@@ -3,7 +3,7 @@ import { CaltalogService } from "../services/catalog.service";
 import { CatalogRepository } from "../repository/catalog.repository";
 import { asyncHandler } from "../utils/asyncHandler";
 import { RequestValidator } from "../utils/requestValidator";
-import { CreateProduct } from "../dto/product.dto";
+import { CreateProduct, UpdateProduct } from "../dto/product.dto";
 
 const router = express.Router();
 
@@ -18,6 +18,68 @@ router.post(
       if (errors) return res.status(400).json(errors);
       const data = await catalogService.createProduct(input);
       return res.status(201).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  })
+);
+
+router.patch(
+  "/products/:id",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { errors, input } = await RequestValidator(UpdateProduct, req.body);
+
+      const id = parseInt(req.params.id) || 0;
+      if (errors) return res.status(400).json(errors);
+
+      const data = await catalogService.updateProduct({ id, ...input });
+      return res.status(200).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  })
+);
+
+router.get(
+  "/products",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const limit = Number(req.query["limit"]);
+    const offset = Number(req.query["offset"]);
+
+    try {
+      const data = await catalogService.getProducts(limit, offset);
+      return res.status(200).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  })
+);
+
+router.get(
+  "/products/:id",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = parseInt(req.params.id) || 0;
+      const data = await catalogService.getProduct(id);
+      return res.status(200).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  })
+);
+
+router.delete(
+  "/products/:id",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const id = parseInt(req.params.id) || 0;
+      const data = await catalogService.deleteProduct(id);
+      return res.status(200).json(data);
     } catch (error) {
       const err = error as Error;
       return res.status(500).json(err.message);
